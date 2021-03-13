f70b94f01437555b70f71a3fd8a45c3e56fdb803
Oliver Otchere <oliverotchere4@gmail.com>
2021-03-13 22:44:28 +0000

    tailwind customization

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        bg_desktop:'url("images/bg-header-desktop.svg")',
+        bg_mobile:'url("images/bg-header-mobile.svg")'
+      },
+      fontFamily: {
+        cfont:'Spartan'
+      },
+      colors:{
+        DesaturatedDarkCyan: 'hsl(180, 29%, 50%)',
+        LightGrayishCyanBackground: 'hsl(180, 52%, 96%)',
+        LightGrayishCyanFilterTablets: 'hsl(180, 31%, 95%)',
+        DarkGrayishCyan: 'hsl(180, 8%, 52%)',
+        VeryDarkGrayishCyan: 'hsl(180, 14%, 20%)'
+      }
+    },
+  },
+  variants: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
